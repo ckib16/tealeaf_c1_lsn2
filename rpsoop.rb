aaba3d157e86fb736975f8c41614441ee3067c79
ckib16 <ckib16@gmail.com>
2014-08-18 21:27:09 -0400

    Finished 1st draft

--- rpsoop.rb
+++ rpsoop.rb
@@ -7,56 +7,109 @@
 # Ask if want to play again
 # If yes loop, if no quite
 
-class Player
+class Hand
+  include Comparable
 
-  attr_accessor :hand, :human_hand, :computer_hand
+  attr_reader :value
 
-  def initialize
-    @@hand = {human_hand: nil, computer_hand: nil}
+  def initialize(v)
+    @value = v
   end
 
-  CHOICES = ["R", "P", "S"]
+  def <=>(another_hand)
+    if @value == another_hand
+      0
+    elsif (@value == 'p' && another_hand.value == 'r') || (@value == 'r' && another_hand.value == 's') ||
+(@value == 's' && another_hand.value == 'p')
+      1
+    else
+      -1
+    end
+  end
+
+  def display_winning_message
+    case @value
+    when 'p'
+      puts "Paper wraps rock!"
+    when 's'
+      puts "Scissors cut paper!"
+    when 'r'
+      puts "Rock bashes Scissors!"
+    end
+  end
 end
 
-class Human < Player
-  attr_accessor :name
+class Player
+
+  attr_accessor :hand
+  attr_reader :name
 
   def initialize(n)
     @name = n
   end
 
-  def human_picks(hp)
-    puts "Chose one: (R/P/S)"
-    begin
-      players_input = gets.chomp.upcase.to_s
-    end until CHOICES.include?(players_input)  # disregard non r/p/s inputs
-    hc[self.human_hand] = players_input
-    puts "You chose #{hc[self.human_hand}"
-    hc
+  def to_s
+    puts "#{player} currently has a choice of #{choice}"
   end
+end
 
+class Human < Player
+  def pick_hand
+    begin
+      puts "Pick one: r / p / s"
+      c = gets.chomp.downcase
+    end until Game::CHOICES.keys.include?(c)
+
+    self.hand = Hand.new(c)
+  end
 end
 
 class Computer < Player
+  def pick_hand
+    self.hand = Hand.new(Game::CHOICES.keys.sample)
+  end
+end
+
+class Game
+
+  CHOICES = {'r' => 'Rock', 'p' => 'Paper', 's' => "Scissors"}
+
+  attr_reader :player, :computer
+
   def initialize
+    @@player_score = 0
+    @@computer_score = 0
+    @player = Human.new('Chris')
+    @computer = Computer.new('Enigma')
+  
   end
 
-  def computer_picks
-    options = ["R", "P", "S"]
-    self.computer_hand = options.sample
-    puts "The computer picks #{computer_choice}."
+  def compare_hands
+
+    if player.hand == computer.hand
+      puts "It's a tie!"
+    elsif player.hand > computer.hand
+      player.hand.display_winning_message
+      @@player_score += 1
+    else 
+      computer.hand.display_winning_message
+      @@computer_score += 1
+    end
+    puts "Player's score is #{@@player_score} and computer's score is #{@@computer_score}."
+  end
+
+  def play
+    begin
+      player.pick_hand
+      puts "#{player.name} picks #{CHOICES[player.hand.value]}"
+      computer.pick_hand
+      puts "#{computer.name} picks #{CHOICES[computer.hand.value]}"
+      compare_hands
+    end until @@computer_score == 3 || @@player_score ==3
   end
 end
 
-class RPS
+Game.new.play
+        
+        
 
-  def start_game
-    puts "welcome to Rock, Paper, Scissors"  
-    puts "What is your name?"
-    name_input = gets.chomp
-    player = Human.new(0, name_input)
-  end
-end
-
-rps = RPS.new
-rps.start_game
